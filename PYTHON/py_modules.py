54a7a70102dd9e437b398b66631534fd7bd3aacb
kirangagitau <dennosea@gmail.com>
2024-01-12 20:04:51 +0300

    importing in-built modules

---
+++ PYTHON/py_modules.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+''' importing math module and renaming it '''
+import math as mt
+
+print(mt.pi)
+print(mt.cos(30))
+
+''' importing isleap from calender module and renaming it'''
+from calendar import isleap as il
+
+print(il(2024))

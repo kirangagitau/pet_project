9320ab045a203a70a2c527d53229ede6636d7504
kirangagitau <dennosea@gmail.com>
2024-01-14 18:30:29 +0300

    math module

---
+++ PYTHON/mathematics.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+import math  # maths in general.
+import statistics  # module for stats.
+import random  # tool for random selection.
+
+print(math.cos(math.pi / 4))
+print(math.log(1024, 2))

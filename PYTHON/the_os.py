4cdff230e48bd37c14e198c282c46f89eaa35c9d
kirangagitau <dennosea@gmail.com>
2024-01-14 18:19:57 +0300

    the operating system interface

---
+++ PYTHON/the_os.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+import os  # importing os module
+'''the os module is one of the largest module in python
+with quite a range of fucntions'''
+
+print(os.getcwd())  # func getcwd to show working dir
+print("\n")
+print(dir(os))  # list all func for os module

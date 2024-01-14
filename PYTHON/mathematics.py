cb0a7ef5002ce05fc87d9640c727e3b1668126b0
kirangagitau <dennosea@gmail.com>
2024-01-14 18:38:59 +0300

    random  module

--- PYTHON/mathematics.py
+++ PYTHON/mathematics.py
@@ -6,3 +6,12 @@ import random  # tool for random selection.
 
 print(math.cos(math.pi / 4))
 print(math.log(1024, 2))
+
+choice = random.choice(['apple', 'pear', 'banan'])  # choose any
+x = random.random()  # random float
+y = random.randrange(20)  # random int from range 20
+
+print('\n')
+print(choice)
+print(x)
+print(y)

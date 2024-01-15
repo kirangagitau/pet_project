fadf39e603c0c403d4b2c175f2dbcee7116f69f2
kirangagitau <dennosea@gmail.com>
2024-01-15 13:14:59 +0300

    import and use module at functions

--- PYTHON/mathematics.py
+++ PYTHON/mathematics.py
@@ -4,14 +4,19 @@ import math  # maths in general.
 import statistics  # module for stats.
 import random  # tool for random selection.
 
-print(math.cos(math.pi / 4))
-print(math.log(1024, 2))
 
-choice = random.choice(['apple', 'pear', 'banan'])  # choose any
-x = random.random()  # random float
-y = random.randrange(20)  # random int from range 20
+def mathematics:
+    print(math.cos(math.pi / 4))
+    print(math.log(1024, 2))
+    return
 
-print('\n')
-print(choice)
-print(x)
-print(y)
+
+def random_module:
+    choice = random.choice(['apple', 'pear', 'banan'])  # choose any
+    x = random.random()  # random float
+    y = random.randrange(20)  # random int from range 20
+    print('\n')
+    print(choice)
+    print(x)
+    print(y)
+    return

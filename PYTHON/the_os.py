a369190a139f151b09cc746184028e76a721f175
kirangagitau <dennosea@gmail.com>
2024-01-17 08:17:29 +0000

    commented the os module

--- PYTHON/the_os.py
+++ PYTHON/the_os.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+'''use python 3 to interpret'''
 
 import os  # importing os module
 '''the os module is one of the largest module in python
@@ -13,7 +14,7 @@ def workingdr():
 def dirlist():
     path = "/"
     dir_list = os.listdir(path)
-    print("File and directories in, " path, "' :")
+    print("File and directories in ", path, "' :")
     print(dir_list)
     return;
 

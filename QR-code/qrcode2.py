f2376ec7fc919e5b59a4ee3f0e4bfb3a9ba78062
kirangagitau <dennosea@gmail.com>
2024-01-17 07:50:53 +0000

    pycode for qr

--- QR-code/qrcode2.py
+++ QR-code/qrcode2.py
@@ -1,4 +1,5 @@
 import qrcode
+
 qr = qrcode.QRCode(
 #VERSION IS SIZE OF MATRIX BTN 1 & 40, 1 IS SMALLEST(21x21)
     version=1,
@@ -7,6 +8,7 @@ qr = qrcode.QRCode(
     box_size=10,
     border=4,
 )
+
 #DATA TO PUT IN QR,ALSO FIT DATA IN THE MATRIX
 qr.add_data('We love group projects')
 qr.make(fit=True)
@@ -14,5 +16,6 @@ qr.make(fit=True)
 #CHANGE BACKGROUND & PAINTING OF QR, THEN SAVE.
 img = qr.make_image(fill_color="black", back_color="white")
 img.save("QR.png")
+
 #NOT SPECIFYING A SAVING DIRECTORY MEANS IMAGE IS SAVED IN THE
 #CURRENT WORKING DIRECTORY OF THE PROJECT

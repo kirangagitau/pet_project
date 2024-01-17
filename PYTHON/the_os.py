7e761cbf45ffad7195540905d3a6bdc1bc1f455d
kirangagitau <dennosea@gmail.com>
2024-01-17 08:10:34 +0000

    the os module

--- PYTHON/the_os.py
+++ PYTHON/the_os.py
@@ -4,6 +4,19 @@ import os  # importing os module
 '''the os module is one of the largest module in python
 with quite a range of fucntions'''
 
-print(os.getcwd())  # func getcwd to show working dir
-print("\n")
-print(dir(os))  # list all func for os module
+def workingdr():
+    print(os.getcwd())  # func to show working dir
+    print("\n")
+    print(dir(os))  # list all func for os module
+    return;
+
+def dirlist():
+    path = "/"
+    dir_list = os.listdir(path)
+    print("File and directories in, " path, "' :")
+    print(dir_list)
+    return;
+
+
+dirlist()
+workingdr()

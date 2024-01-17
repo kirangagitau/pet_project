1c500fca284589f6b2b9c32a397493d6515c5f96
kirangagitau <dennosea@gmail.com>
2024-01-17 18:17:54 +0300

    conformed the_os.py with pycodestyles

--- PYTHON/the_os.py
+++ PYTHON/the_os.py
@@ -5,18 +5,20 @@ import os  # importing os module
 '''the os module is one of the largest module in python
 with quite a range of fucntions'''
 
+
 def workingdr():
     print(os.getcwd())  # func to show working dir
     print("\n")
     print(dir(os))  # list all func for os module
-    return;
+    return
+
 
 def dirlist():
     path = "/"
     dir_list = os.listdir(path)
     print("File and directories in ", path, "' :")
     print(dir_list)
-    return;
+    return
 
 
 dirlist()

cb713b6acb81a1584307c76708a21e0c0b076eb1
kirangagitau <dennosea@gmail.com>
2024-01-28 20:27:11 +0300

    coding is fun

--- PYTHON/modulo.py
+++ PYTHON/modulo.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
 
-from cal import *
+from cal import *  # import all functions
+
 
 def mathing():
     add(5, 7)
     sub(5, 7)
     return
 
+
 mathing()
+
+''' Functions are best ways to solve for
+complexity as a problem can be broken down
+into smaller simpler problems or tasks where
+each task get worked on individually and the
+overall result combined back together.'''

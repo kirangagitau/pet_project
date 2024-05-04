9ebc5064b9e689cfe60134331c234de88478db65
gitau <dennosea@gmail.com>
2024-05-04 05:01:04 +0300

    if_else in function

--- unit-convert.py
+++ unit-convert.py
@@ -1,9 +1,25 @@
 #!/usr/bin/python3
 
+''' A function to convert a given
+weight into either pounds of kilograms
+'''
+
+
 def weight_unit():
     mass = float(input("Enter mass: "))
-    print("k:kilogram, l= lb")
+    choice = input(("k= kilograms, l= lb: "))
+    if choice.upper() == "K":
+        ''' upper() converts choice to upper case
+        before doing comparison'''
+
+        lb = mass * 2.205  # convert to pounds
+        print('Mass is {:.2f}'.format(lb))
+    elif choice.lower() == "l":
+        kg = mass * 0.45  # convert to kg
+        print('Mass is {:.2f}'.format(kg))  # formated output
+    else:
+        print("INVALID INPUT !!")
     return
 
 
-weight_unit()
+weight_unit()  # calling the function
